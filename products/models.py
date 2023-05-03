180c0b7ec9c10250a1f244c173ae9af495f3a836
aokealy <aokealy@gmail.com>
2023-05-03 12:59:11 +0000

    add products and fix admin page

--- products/models.py
+++ products/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     readable_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,6 +16,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=250)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)

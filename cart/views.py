8970d98f3290a1a04cba7df2fce757de2d967664
aokealy <aokealy@gmail.com>
2023-05-04 13:41:34 +0000

    add condition to checkout page

--- cart/views.py
+++ cart/views.py
@@ -12,11 +12,25 @@ def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    condition = None
+    if 'product_condition' in request.POST:
+        condition = request.POST['product_condition']
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if condition:
+        if item_id in list(cart.keys()):
+            if condition in cart[item_id]['items_by_condition'].keys():
+                cart[item_id]['items_by_condition'][condition] += quantity
+            else:
+                cart[item_id]['items_by_condition'][condition] = quantity
+        else:
+            cart[item_id] = {'items_by_condition': {condition: quantity}}
     else:
+
+
+     if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+     else:
         cart[item_id] = quantity
 
     request.session['cart'] = cart

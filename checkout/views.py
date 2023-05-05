666f57425b4c65b31cef0a2e0abc9e7e5d083e67
aokealy <aokealy@gmail.com>
2023-05-05 11:07:21 +0000

    try add stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There is nothing in your cart currently")
         return redirect(reverse('products'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
     order_form = OrderForm()
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,

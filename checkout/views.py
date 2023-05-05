10c8c5791ad454a4e18246a0d94d4e64bfabe051
aokealy <aokealy@gmail.com>
2023-05-05 15:23:35 +0000

    add stripe

--- checkout/views.py
+++ checkout/views.py
@@ -9,16 +9,12 @@ from cart.contexts import cart_contents
 
 
 def checkout(request):
-
-
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There is nothing in your cart currently")
         return redirect(reverse('products'))
 
-    current_cart = cart_contents(request)
-    total = current_cart['grand_total']
-   
+    
     
     
 
@@ -26,6 +22,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MzNKpBmLoLbFeg9WvOlyYyrMcfTxJTzI9JYSuhtUpbZDgPyPbNoZoVekVRk1nFqvOe7SKgKqtuZ56LikeA4HZ6u006w6r6kRB',
+        'client_secret': 'test client secret',
         
        
     }

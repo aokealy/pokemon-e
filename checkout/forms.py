10d84978dc55ac905f7e233a8009aeb837530a37
aokealy <aokealy@gmail.com>
2023-05-05 14:50:42 +0000

    can't fix stripe

--- checkout/forms.py
+++ checkout/forms.py
@@ -11,6 +11,10 @@ class OrderForm(forms.ModelForm):
                   'county',)
 
     def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
         super().__init__(*args, **kwargs)
         placeholders = {
             'full_name': 'Full Name',
@@ -18,7 +22,7 @@ class OrderForm(forms.ModelForm):
             'phone_number': 'Phone Number',
             'country': 'Country',
             'postcode': 'Postal Code',
-            'city': 'City',
+            'city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County',
--- checkout/views.py
+++ checkout/views.py
@@ -5,12 +5,10 @@ from django.conf import settings
 from .forms import OrderForm
 from cart.contexts import cart_contents
 
-import stripe
+
 
 
 def checkout(request):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
 
     cart = request.session.get('cart', {})
@@ -20,20 +18,16 @@ def checkout(request):
 
     current_cart = cart_contents(request)
     total = current_cart['grand_total']
-    order_form = OrderForm()
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
-    print(intent)
+   
+    
+    
 
+    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MzNKpBmLoLbFeg9Wc00RkY2okfX1armtPbUn6mVCEo7aJPcC0CaHfso2VfPcLTbMyQbHc8xGRAtkSWxnFhPmSjj00EGk8pp05',
-        'client_secret': 'test client secret',
+        
+       
     }
 
     return render(request, template, context)

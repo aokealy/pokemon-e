ce0b7026772d68517c291014fc3e4626e6c7871a
aokealy <aokealy@gmail.com>
2023-05-08 10:51:47 +0000

    add country fields for checkout page

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,9 +20,8 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
-            'city': 'Town or City',
+            'city': 'City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County',
@@ -30,6 +29,7 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
+         if field != 'country':
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
             else:

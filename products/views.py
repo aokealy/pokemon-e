498d481741fa255f06097cbc2debd8fc559a5c7c
aokealy <aokealy@gmail.com>
2023-05-08 18:04:51 +0000

    add product management fields to add products and create url and views with templates for it

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from .forms import ProductForm
 # Create your views here.
 
 def all_products(request):
@@ -65,3 +66,13 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
